cce39c4af038e0e2d3275826229037179ea02364
Shanqiang Ke <nosqlcoco@gmail.com>
2016-10-15 13:46:20 +0800

    添加测试类

---
+++ src/test/java/com/weapp/ApplicationTest.java
@@ -0,0 +1,44 @@
+package com.weapp;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.weapp.controller.WxController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class ApplicationTest {
+	
+	private MockMvc mvc;
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(
+				new WxController()).build();
+				
+	}
+	//测试获取数据接口
+	@Test
+	public void test() throws Exception{
+		RequestBuilder request = null;
+		request = post("/test")
+				.param("name", "xiaoqiang");
+				
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("{\"content\":\"Your name is xiaoqiang\"}")));
+	}
+}
